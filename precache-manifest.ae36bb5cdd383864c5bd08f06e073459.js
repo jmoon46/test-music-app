343e28cb7a7017810cd53faeff90846bdb838030
John <jrmint46@gmail.com>
2020-06-30 22:54:23 -0400

    Updates

---
+++ precache-manifest.ae36bb5cdd383864c5bd08f06e073459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb87388fbd4dd9bd393edb37d8733995",
+    "url": "/modal-chart/index.html"
+  },
+  {
+    "revision": "266a7fc25c4313ac035e",
+    "url": "/modal-chart/static/css/main.b31cb7e0.chunk.css"
+  },
+  {
+    "revision": "548b3864aa90bac79e52",
+    "url": "/modal-chart/static/js/2.46265caa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/modal-chart/static/js/2.46265caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266a7fc25c4313ac035e",
+    "url": "/modal-chart/static/js/main.6711dcb4.chunk.js"
+  },
+  {
+    "revision": "130063c8f24ca0e7f1cd",
+    "url": "/modal-chart/static/js/runtime-main.057db8f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-music-app/precache-manifest.e16996c27a83b499aba890472cd809f2.js"
+  "/modal-chart/precache-manifest.ae36bb5cdd383864c5bd08f06e073459.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-music-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modal-chart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
